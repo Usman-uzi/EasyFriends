fc8057afb6a1502ea48803ac2f688a4d2be5ee8b
Usman <usman.solemn@gmail.com>
2016-04-12 09:25:57 +0500

    updates
    updates

---
+++ EFLibrary/String/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLibrary.String
+{
+    public class Utility
+    {
+        public static string RandomString(int stringLength, Random rd)
+        {
+            const string allowedChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789";
+            char[] chars = new char[stringLength];
+
+            for (int i = 0; i < stringLength; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+}

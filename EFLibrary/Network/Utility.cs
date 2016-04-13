317123db94e510f5457d2b76fc6fc2ae8b463f88
Usman <usman.solemn@gmail.com>
2016-04-13 11:19:11 +0500

    Network Utilities introduction

---
+++ EFLibrary/Network/Utility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EFLibrary.Network
+{
+    public class Utility
+    {
+        public static string GetClientIP()
+        {
+            return HttpContext.Current.Request.UserHostAddress;
+
+        }  
+    }
+}

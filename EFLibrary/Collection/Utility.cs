cdecb5f45e7b98112e2b9415edaf745bbd1c49a0
Usman <usman.solemn@gmail.com>
2016-04-13 16:49:58 +0500

    Updates

---
+++ EFLibrary/Collection/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLibrary.Collection
+{
+    public class Utility
+    {
+      
+    /// <summary>
+    /// shuffle the array items randomly
+    /// </summary>
+    /// <param name="list"></param>
+    /// <returns></returns>
+    public IList<object> shuffleArray  (List<object> list)
+    {
+        int currentIndex = list.Count;
+        object temporaryValue;
+        int total = list.Count;           
+        int randomIndex;
+        while (0 != currentIndex) {
+            var random = new Random();
+            randomIndex = random.Next(total)* currentIndex;
+            currentIndex -= 1;
+            temporaryValue = list[currentIndex];
+            list[currentIndex] = list[randomIndex];
+            list[randomIndex] = temporaryValue;
+        }
+        return list;        
+    }
+    }
+}
--- EFLibrary/Network/Utility.cs
+++ EFLibrary/Network/Utility.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -13,6 +15,55 @@ namespace EFLibrary.Network
         {
             return HttpContext.Current.Request.UserHostAddress;
 
-        }  
+        }
+
+        public static bool SendEmail()
+        {
+            try
+            {
+                string smtpAddress = "smtp.mail.yahoo.com";
+                int portNumber = 587;
+                bool enableSSL = true;
+
+                string emailFrom = "email@yahoo.com";
+                string password = "abcdefg";
+                string emailTo = "someone@domain.com";
+                string subject = "Hello";
+                string body = "Hello, I'm just writing this to say Hi!";
+
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(emailFrom);
+                    mail.To.Add(emailTo);
+                    mail.Subject = subject;
+                    mail.Body = body;
+                    mail.IsBodyHtml = true;
+                    // Can set to false, if you are sending pure text.
+
+                    mail.Attachments.Add(new Attachment("C:\\SomeFile.txt"));
+                    mail.Attachments.Add(new Attachment("C:\\SomeZip.zip"));
+
+                    using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                    {
+                        smtp.Credentials = new NetworkCredential(emailFrom, password);
+                        smtp.EnableSsl = enableSSL;
+                        smtp.Send(mail);
+                    }
+
+                }
+            }
+            catch (Exception exp)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public enum MailServer
+    {
+        Gmail,
+        Yahoo,
+        Outlook
     }
 }
